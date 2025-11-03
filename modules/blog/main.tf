c3c5d647ac52fc81dd0e602d1c8c7440769244d5
Tri Dinh Le <91708260+triledinh159@users.noreply.github.com>
2025-11-03 17:02:47 +0700

    change the ver for var target_group_arns

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -34,13 +34,14 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
+  version = "~> 8.0.0"
 
   name = "blog"
 
   min_size            = var.asg_min
   max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arn    = module.blog_alb.target_group_arn
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
